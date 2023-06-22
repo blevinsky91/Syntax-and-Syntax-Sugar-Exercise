db2308f07acb67373f1dd6d125a2beedb60a307a
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-22 14:30:41 -0400

    completed exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            //TO DO:
+            //change to inferred typing
+            //use string interpolation
+            //use ternary operator
+
+            //answer below(changed)
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            
+            //original code
+            //if (answer < 9)
+            //{
+                //response = answer + "is less than nine";
+            //}
+            //else
+            //{
+                //response = answer + "greater than or equal to nine";
+            //}
+            Console.WriteLine(response);
         }
     }
 }
